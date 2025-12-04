1e1886172128cd9be9b94a07aacd5bba22fbc98c
Zacarias <aprigio.rj@gmail.com>
2025-12-04 17:03:18 -0300

    Corrigido nome da função PDF

--- src/routes/pdfRoutes.ts
+++ src/routes/pdfRoutes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import { auth } from "../middlewares/auth";
-import { permitir } from "../middlewares/permissoes";
 import * as controller from "../controllers/pdfController";
 
 const router = Router();
 
-router.get("/:cliente_id", auth, permitir("gerar_pdf"), controller.gerarHistoricoPDF);
+router.get("/:id", controller.gerarPDF);
 
 export default router;
